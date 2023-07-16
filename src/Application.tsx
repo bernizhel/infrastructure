689073a415a53b5bfc8d6971fba1ccbc001afeda
bernizhel <detrop.1995@gmail.com>
2023-07-16 12:06:52 +0300

    ci: trying to fix deployments

--- src/Application.tsx
+++ src/Application.tsx
@@ -11,14 +11,14 @@ export const Application: FC = () => {
   return (
     <div>
       <nav>
-        <Link data-testid="link-home" to={`${process.env.PUBLIC_URL}/`}>
+        <Link data-testid="link-home" to="/">
           Home
         </Link>
-        <Link to={`${process.env.PUBLIC_URL}/about`}>About</Link>
+        <Link to="/about">About</Link>
       </nav>
       <Routes>
-        <Route path={`${process.env.PUBLIC_URL}/about`} element={about} />
-        <Route path={`${process.env.PUBLIC_URL}/`} element={home} />
+        <Route path="/about" element={about} />
+        <Route path="/" element={home} />
       </Routes>
     </div>
   );
